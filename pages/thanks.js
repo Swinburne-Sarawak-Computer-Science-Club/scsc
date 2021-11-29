08aaed20cfa0e1d20ac6c094ad743c425ee63c0b
Jehovah Yii Zui Hon (Hovah) <31881261+hovahyii@users.noreply.github.com>
2021-11-29 14:21:17 +0800

    update Hovah link

--- pages/thanks.js
+++ pages/thanks.js
@@ -32,7 +32,7 @@ const credits = () => {
                 <p>Our thanks to SCSC’s committees and friends in industry and beyond, including:</p>
 
               <ul>
-                    <li data-marker="*"><a href="https://hovahyii.github.io/hovah.io/">Hovah Yii</a>, created and sponsored the SCSC website</li>
+                    <li data-marker="*"><a href="https://github.com/hovahyii">Hovah Yii</a>, created and sponsored the SCSC website</li>
                     <li data-marker="*"><a href="https://github.com/Laikokwui">Lai Kok Wei</a>, maintained and updated the SCSC website</li>
                     <li data-marker="*"><a href="https://www.facebook.com/aylwinsim.chunwei">Alywin Sim</a>, contributed idea for the SCSC website</li>
                     <li data-marker="*"><a href="https://www.facebook.com/markloveuguysnJesus">Mark Goh</a>, helped with the SCSC website</li>

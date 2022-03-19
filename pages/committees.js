3f83b6635449aa6bb4c995553d56c4df73cf07d7
Jehovah Yii Zui Hon (Hovah) <31881261+hovahyii@users.noreply.github.com>
2022-03-19 17:35:08 +0800

    Latest Update: 1. Fix NextAuth login issue 2. Finish authentication to Admin Dashboard 3. Added new committee 2022

--- pages/committees.js
+++ pages/committees.js
@@ -4,41 +4,47 @@ import Head from "next/head";
 
 const committees = () => {
   return (
+		<body>
+			<Head>
+				<title>Committees</title>
+				<meta
+					name="description"
+					content="All about SCSC founders, previous committee, the Jedi Council until now"
+				/>
+				<meta
+					name="keywords"
+					content="committee, scsc committee, the jedi council, the founder"
+				/>
+				<meta name="author" content="Hovah Yii" />
+			</Head>
 
-    <body>
-  
+			<Js />
 
-      <Head>
-      <title>Committees</title>
-      <meta name="description" content="All about SCSC founders, previous committee, the Jedi Council until now" />
-      <meta name="keywords" content="committee, scsc committee, the jedi council, the founder" />
-      <meta name="author" content="Hovah Yii"/>
-      </Head>
+			<div className="container-fluid">
+				<div className="row">
+					<Nav />
 
-      <Js />
-      
-    <div className="container-fluid">
-
-    <div className="row">
-      
-      <Nav />
-
-   
-    <main class="col-md markdown-body">
-
-            <h2><a href="/committees/2018/">2018</a></h2>
-            <h2><a href="/committees/2019/">2019</a></h2>
-            <h2><a href="/committees/2020/">2020</a></h2>
-            <h2><a href="/committees/2021/">2021</a></h2>
-
-    </main>
-
-</div>
-
-</div>
-</body>
-
-  )
+					<main class="col-md markdown-body">
+						<h2>
+							<a href="/committees/2018/">2018</a>
+						</h2>
+						<h2>
+							<a href="/committees/2019/">2019</a>
+						</h2>
+						<h2>
+							<a href="/committees/2020/">2020</a>
+						</h2>
+						<h2>
+							<a href="/committees/2021/">2021</a>
+						</h2>
+						<h2>
+							<a href="/committees/2022/">2022</a>
+						</h2>
+					</main>
+				</div>
+			</div>
+		</body>
+	)
 }
 
 export default committees
